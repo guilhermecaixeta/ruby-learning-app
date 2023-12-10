365a64e93b245c98dd95f9e4fd0f3c1b6f9b6cb4
Guilherme Caixeta <guilherme.lpcaixeta@gmail.com>
2023-12-10 16:32:14 +0000

    adding devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# An example of post create file for a devcontainer
+
+# Source ASDF
+. /usr/local/asdf/asdf.sh
+
+echo "Installing rails" 
+gem install rails 6.0
+
+echo "Installing yarn" 
+npm --global install yarn 
+
+# Install the version of Bundler specified in Gemfile.lock
+if [ -f Gemfile.lock ]; then
+    bundler_version=$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1 | tr -d ' ')
+    gem install bundler -v "$bundler_version"
+fi
+
+# If there's a Gemfile, then run `bundle install`
+if [ -f Gemfile ]; then
+    echo "installing bundle"
+    bundle install
+    
+    echo "yarn checking files"
+    yarn install --check-files
+fi
